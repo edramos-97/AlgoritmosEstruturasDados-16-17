3d6875c367332e533ea9ad2f9d172a995225c96e
rjstorres <r_torres96@live.com.pt>
2016-11-18 16:53:00 +0000

    Person.h doxygen ready. Minor changes. Ver comentario em Stundent::add_approved_credits posso estar errado

--- teste1/TESTE1/person.cpp
+++ teste1/TESTE1/person.cpp
@@ -1,13 +1,73 @@
 #include "person.h"
 
+/*********************************
+*CLASS PERSON
+*********************************/
+
+Person::Person(string code, string name) : code(code), name(name) {
+	if (name.size() < 3)
+		throw short_argument(name, "Nome que tentou atribuir a esta pessoa é demasiado pequeno");
+	if (code.length() != 11)
+		throw short_argument(code, "Codigo de aluno ou tutor invalido");
+}
+
+/*********************************
+*CLASS TUTOR
+*********************************/
+Tutor::Tutor(string code, string name) : Person(code, name) {
+	if(code.at(0)!='t'||code.at(1)!='u')
+		throw short_argument(code, "Codigo de tutor invalido");
+}
+
+string Tutor::get_name() const { return name; }
+
+string Tutor::get_code() const { return code; }
+
 void Tutor::add_student(Student* x)
 {
 	students.push_back(x);
 }
 
-const vector<string> Student::get_enrol_courses() const {
+bool Tutor::operator< (const Tutor &t) const {
+	return students.size() < t.students.size();
+}
+
+/*********************************
+*CLASS Student
+*********************************/
+
+Student::Student(string code, string name, string email, string status, string tutor) : Person(code, name), email(email), status(status), tutor(tutor) {
+	approved_credits = 0;
+	credits = 0;
+	if (code.at(0) != 's' || code.at(1) != 't')
+		throw short_argument(code, "Codigo de student invalido");
+}
+
+Student::Student(string code, string name, string email, string status) : Person(code, name), email(email), status(status) {
+	approved_credits = 0;
+	credits = 0;
+	if (code.at(0) != 's' || code.at(1) != 't')
+		throw short_argument(code, "Codigo de student invalido");
+}
+
+string Student::get_name() const { return name; }
+string Student::get_code() const { return code; }
+string Student::get_email() const { return email; }
+string Student::get_status() const { return status; }
+string Student::get_tutor() const { return tutor; }
+
+vector<string> Student::get_enrol_courses() const {
 	return enrolled_courses;
 }
+
+double Student::get_appcredits() const { return approved_credits; }
+double Student::get_credits() const { return credits; }
+void Student::add_approved_credits(double x) { 
+	approved_credits += x;
+	credits -= x; // right? se foi aprovado retiramos dos creditos que esta inscrito...
+}
+void Student::add_credits(double x) { credits += x; }
+void Student::assign_tutor(Tutor * x) { tutor = x->get_code(); }
 
 ostream & operator<<(ostream & os, const Student & s)
 {
@@ -17,8 +77,4 @@ ostream & operator<<(ostream & os, const Student & s)
 		"Completed credits: " << s.approved_credits << endl <<
 		"Credits currently enrolled for: " << s.credits;
 	return os;
-}
-
-bool Tutor::operator< (const Tutor &t) const {
-	return students.size() < t.students.size();
 }
