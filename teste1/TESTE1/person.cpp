dd586c0a873bd6b56523587e3694c220282ee404
Esteves <up201505145>
2016-11-20 12:36:20 +0000

    - Student::approve_course() no longer subtracts from credits if student wasn't enrolled in course.

--- teste1/TESTE1/person.cpp
+++ teste1/TESTE1/person.cpp
@@ -79,15 +79,19 @@ void Student::enroll_course(Course *course) {
 }
 
 void Student::approve_course(Course *course) {
+	bool found = false;
 	for (vector<Course *>::iterator it = enrolled_courses.begin(); it != enrolled_courses.end(); ++it) {
 		if (*it == course) {
 			enrolled_courses.erase(it);
+			found = true;
 			break;
 		}
 	}
 
 	approved_courses.push_back(course);
-	credits -= course->get_credits();
+	if (found) {
+		credits -= course->get_credits();
+	}
 	approved_credits += course->get_credits();
 }
 
