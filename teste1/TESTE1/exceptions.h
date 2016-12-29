3b8d538ac8b84206dafb41aac1bcb9883f3b39d7
Esteves <up201505145>
2016-12-29 11:56:31 +0000

    exceptions.h: Finished documentation.

--- teste1/TESTE1/exceptions.h
+++ teste1/TESTE1/exceptions.h
@@ -1,8 +1,8 @@
 /**
 * @file exceptions.h
 * @author AEDA T4GE
-* @title exception class used on this project
-* @brief exception class used on this project
+* @title Generic exception class
+* @brief Exception class used throughout the project.
 */
 #pragma once
 
@@ -14,43 +14,46 @@ class exception_or_error {
 	string reason;
 public:
 	/**
-	*@brief constructor of the exception class
-	*@param reason  reason that caused a throw
+	*@brief Creates an exception with a given reason.
+	*@param reason Reason for exception throw.
 	*/
 	exception_or_error(string reason) : reason(reason) {};
+
 	/**
-	*@brief returns reason that caused a throw
-	*@return  returns reason that caused a throw
+	*@brief Gets exception throw's reason.
+	*@return Reason for exception throw.
 	*/
 	string get_reason() { return reason; };
 };
 
-class NoSlots {
-	uint year;
-public:
-	/**
-	*@brief constructor of the exception class
-	*@param year year of the class with no slots available
-	*/
-	NoSlots(uint year) { this->year = year; }
-	/**
-	*@brief returns the year data-member
-	*@return year data-member
-	*/
-	uint getYear() const { return year;	}
-};
 
-class NoClasses {
-	uint year;
-public:
-	/**
-	*@brief constructor of the exception class
-	*@param year year that has no available class
-	*/
-	NoClasses(uint year) { this->year = year; }
-	/**
-	*@brief returns the year data-member
-	*@return year data-member
-	*/
-	uint getYear() const { return year; }
-};
+// Unused exceptions.
+//class NoSlots {
+//	uint year;
+//public:
+//	/**
+//	*@brief constructor of the exception class
+//	*@param year year of the class with no slots available
+//	*/
+//	NoSlots(uint year) { this->year = year; }
+//	/**
+//	*@brief returns the year data-member
+//	*@return year data-member
+//	*/
+//	uint getYear() const { return year;	}
+//};
+//
+//class NoClasses {
+//	uint year;
+//public:
+//	/**
+//	*@brief constructor of the exception class
+//	*@param year year that has no available class
+//	*/
+//	NoClasses(uint year) { this->year = year; }
+//	/**
+//	*@brief returns the year data-member
+//	*@return year data-member
+//	*/
+//	uint getYear() const { return year; }
+//};
